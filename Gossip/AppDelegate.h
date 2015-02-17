2207cb88c30588560ca7f8093867d9d443306042
Yosamac <yosamac@gmail.com>
2015-02-17 13:13:43 +0100

    Initial Commit

---
+++ Gossip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gossip
+//
+//  Created by Yosnier on 17/02/15.
+//  Copyright (c) 2015 YOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
