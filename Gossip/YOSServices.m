fba96c212c2be04239baa05ee782fcd6f55a5f4a
Yosamac <yosamac@gmail.com>
2015-03-17 22:00:09 +0100

    Adding services

--- Gossip/YOSServices.m
+++ Gossip/YOSServices.m
@@ -10,6 +10,7 @@
 #import "YOSService.h"
 #import "YOSPhotoContainer.h"
 #import "YOSAppDelegate.h"
+#import "Settings.h"
 
 
 @implementation YOSServices
@@ -21,28 +22,53 @@
     
     if (self = [super init]) {
         
-        YOSAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
         
-        YOSPhotoContainer  *photoGit = [YOSPhotoContainer  insertInManagedObjectContext:appDelegate.stack.context];
+        
+        YOSPhotoContainer  *photoGit = [YOSPhotoContainer  insertInManagedObjectContext:STACK.context];
         photoGit.image = [UIImage imageNamed:@"octocat.png"];
         [YOSService serviceWithName:@"GitHub"
                              detail:@"the nerd's facebook"
                               photo: photoGit
-                            context:appDelegate.stack.context];
+                            context:STACK.context];
         
-        YOSPhotoContainer  *photoGoogle = [YOSPhotoContainer  insertInManagedObjectContext:appDelegate.stack.context];
+        YOSPhotoContainer  *photoGoogle = [YOSPhotoContainer  insertInManagedObjectContext:STACK.context];
         photoGoogle.image = [UIImage imageNamed:@"googleDrive.png"];
         [YOSService serviceWithName:@"Google Drive"
                              detail:@"Data store in the cloud 1"
                               photo:photoGoogle
-                            context:appDelegate.stack.context];
+                            context:STACK.context];
         
-        YOSPhotoContainer  *photoDropbox = [YOSPhotoContainer  insertInManagedObjectContext:appDelegate.stack.context];
+        YOSPhotoContainer  *photoDropbox = [YOSPhotoContainer  insertInManagedObjectContext:STACK.context];
         photoDropbox.image = [UIImage imageNamed:@"dropbox.png"];
         [YOSService serviceWithName:@"Dropbox"
                              detail:@"Data store in the cloud 2"
                               photo: photoDropbox
-                            context:appDelegate.stack.context];
+                            context:STACK.context];
+        
+        
+        YOSPhotoContainer  *photoMendeley = [YOSPhotoContainer  insertInManagedObjectContext:STACK.context];
+        photoMendeley.image = [UIImage imageNamed:@"mendeley.png"];
+        [YOSService serviceWithName:@"Mendeley"
+                             detail:@"Social Reference manager"
+                              photo: photoMendeley
+                            context:STACK.context];
+        
+        
+        YOSPhotoContainer  *photoGoogleCalendar = [YOSPhotoContainer  insertInManagedObjectContext:STACK.context];
+        photoGoogleCalendar.image = [UIImage imageNamed:@"calendar.png"];
+        [YOSService serviceWithName:@"Google Calendar"
+                             detail:@"Your calendars"
+                              photo: photoGoogleCalendar
+                            context:STACK.context];
+        
+        
+        YOSPhotoContainer  *photoEvernote = [YOSPhotoContainer  insertInManagedObjectContext:STACK.context];
+        photoEvernote.image = [UIImage imageNamed:@"evernote_logo_center_4c-sm.png"];
+        [YOSService serviceWithName:@"Evernote"
+                             detail:@""
+                              photo: photoEvernote
+                            context:STACK.context];
+        
     }
     return self;
 }
